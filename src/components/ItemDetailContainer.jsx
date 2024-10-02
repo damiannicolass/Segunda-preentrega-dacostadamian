a46ce84b93e67c231ba62e2e52a86bc4f005f894
Damian Nicolas da Costa <damiannicolasdacosta@gmail.com>
2024-10-02 08:56:29 -0300

    correccion use param

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom'; 
-import { objetId } from "../asyncMock";
+import objets from "../asyncMock"; 
 import ItemDetail from './ItemDetail';
 
 const ItemDetailContainer = () => {
@@ -8,11 +8,16 @@ const ItemDetailContainer = () => {
     const [item, setItem] = useState(null);
 
     useEffect(() => {
-        console.log("Fetching product with id:", productId);
-        objetId(parseInt(productId))  
+        console.log("Fetching products...");
+        objets()
             .then((res) => {
-                console.log("Product found:", res);
-                setItem(res);
+                const productFound = res.find(product => product.id === parseInt(productId)); 
+                if (productFound) {
+                    console.log("Product found:", productFound);
+                    setItem(productFound);
+                } else {
+                    console.error("Product not found");
+                }
             })
             .catch((error) => {
                 console.error("Error fetching product:", error);
